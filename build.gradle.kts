01782659d4f4e2c35909a4091f1b466bb9a52a31
Geraldo <geraldo.gja@gmail.com>
2025-10-14 23:20:17 +0100

    build.gradle - Lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
