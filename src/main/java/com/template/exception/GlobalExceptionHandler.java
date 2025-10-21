ef4eb40d1261fa2c726709450d636f433ce54ea1
Geraldo <geraldo.gja@gmail.com>
2025-10-21 22:32:13 +0100

    GlobalExceptionHandler

--- src/main/java/com/template/exception/GlobalExceptionHandler.java
+++ src/main/java/com/template/exception/GlobalExceptionHandler.java
@@ -1,19 +1,20 @@
 package com.template.exception;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.log4j.Log4j2;
+
+import jakarta.persistence.EntityNotFoundException;
+
+import java.nio.file.AccessDeniedException;
 import org.apache.coyote.BadRequestException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-
-import java.nio.file.AccessDeniedException;
-import java.time.LocalDateTime;
 
 
 @Log4j2
@@ -35,12 +36,12 @@ public class GlobalExceptionHandler {
 
     private HttpStatus determineHttpStatus(Throwable ex) {
         return switch (ex) {
-            case EntityNotFoundException entityNotFoundException -> HttpStatus.NOT_FOUND;
-            case DataIntegrityViolationException dataIntegrityViolationException -> HttpStatus.CONFLICT;
-            case MethodArgumentNotValidException methodArgumentNotValidException -> HttpStatus.BAD_REQUEST;
-            case BadRequestException badRequestException -> HttpStatus.BAD_REQUEST;
-            case AccessDeniedException accessDeniedException -> HttpStatus.FORBIDDEN;
-            case EmptyResultDataAccessException emptyResultDataAccessException -> HttpStatus.OK;
+            case EntityNotFoundException ignored -> HttpStatus.NOT_FOUND;
+            case DataIntegrityViolationException ignored -> HttpStatus.CONFLICT;
+            case MethodArgumentNotValidException ignored -> HttpStatus.BAD_REQUEST;
+            case BadRequestException ignored -> HttpStatus.BAD_REQUEST;
+            case AccessDeniedException ignored -> HttpStatus.FORBIDDEN;
+            case EmptyResultDataAccessException ignored -> HttpStatus.OK;
             case null, default -> HttpStatus.INTERNAL_SERVER_ERROR;
         };
     }
