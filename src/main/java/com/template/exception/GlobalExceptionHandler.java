ade3a3834a0b7460db8a4f5408ab77b4bef04edf
Geraldo <geraldo.gja@gmail.com>
2025-10-23 20:12:07 +0100

    GlobalExceptionHandler update

--- src/main/java/com/template/exception/GlobalExceptionHandler.java
+++ src/main/java/com/template/exception/GlobalExceptionHandler.java
@@ -6,8 +6,10 @@ import jakarta.persistence.EntityNotFoundException;
 
 import java.nio.file.AccessDeniedException;
 import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import org.springframework.http.HttpStatus;
@@ -21,9 +23,15 @@ import org.springframework.web.context.request.WebRequest;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @Value("${logging.colors.enabled:false}")
+    private boolean colorsEnabled;
+
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<ErrorResponse> handleException(Throwable ex, WebRequest request) {
-        log.error(ex);
+        String message = ex.getMessage() != null ? ex.getMessage() : "An unexpected error occurred";
+        return buildErrorMessage(ex, request, message);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex, WebRequest request) {
 
@@ -38,14 +46,15 @@ public class GlobalExceptionHandler {
         return buildErrorMessage(ex, request, message);
     }
 
+    private ResponseEntity<ErrorResponse> buildErrorMessage(Throwable ex, WebRequest request, String message) {
+        printErrorLog(ex);
+
         HttpStatus status = determineHttpStatus(ex);
-        String message = ex.getMessage() != null ? ex.getMessage() : "Internal Error";
         String path = request.getDescription(false).replace("uri=", "");
 
         ErrorResponse response = new ErrorResponse(status.value(), message, path);
         return new ResponseEntity<>(response, status);
     }
-
 
     private HttpStatus determineHttpStatus(Throwable ex) {
         return switch (ex) {
@@ -57,6 +66,15 @@ public class GlobalExceptionHandler {
             case EmptyResultDataAccessException ignored -> HttpStatus.OK;
             case null, default -> HttpStatus.INTERNAL_SERVER_ERROR;
         };
+    }
+
+    private void printErrorLog(Throwable ex) {
+        if (colorsEnabled) {
+            String RED = "\u001B[31m";
+            String RESET = "\u001B[0m";
+            log.error("{}Exception: {}{}", RED, ex.getMessage(), RESET);
+        }else
+            log.error("Exception: {}", ex.getMessage(), ex);
     }
 
 }
